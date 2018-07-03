96bf1b1a9a83d5980b7fe78d4a89f493905f002e
harry_squater <kawka@lefthandfreestudio.com>
2018-07-03 14:09:19 +0200

    9th finished clean

--- solved/9.py
+++ solved/9.py
@@ -1,1 +1,18 @@
-if __name__ == "__main__":		
+def findPytTriplet():
+	index = 1000
+	a = list(range(1,1001))
+	for i in a:
+		toSplit = 1000 - i
+		b = 0
+		while b <= toSplit:
+			c = toSplit - b
+			if i**2 + b**2 == c**2:
+				product = i*b*c
+				print(i)
+				print(b)
+				print(c)
+				return product
+			b += 1
+
+if __name__ == "__main__":
+	print(findPytTriplet())		
